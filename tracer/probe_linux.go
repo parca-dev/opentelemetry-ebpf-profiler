8ea25c4a5a42ae2f6d42294c44e85aa9cb21cde4
Brennan Vincent <brennan@umanwizard.com>
2026-02-17 15:01:52 -0700

    Get rid of tracer.GetCurrentKernelVersion in favor of util.
    (Why do we have this change? Should we just revert it in our fork?)
    
    (Also, even if we DO keep this, we should squash it into whatever
    commit added util.GCKV)

--- tracer/probe_linux.go
+++ tracer/probe_linux.go
@@ -7,9 +7,7 @@
 package tracer // import "go.opentelemetry.io/ebpf-profiler/tracer"
 
 import (
-	"bytes"
 	"errors"
-	"fmt"
 
 	"golang.org/x/sys/unix"
 )
@@ -21,15 +19,4 @@ func ProbeBPFSyscall() error {
 		return errors.New("eBPF syscall is not available on your system")
 	}
 	return nil
-}
-
-// GetCurrentKernelVersion returns the major, minor and patch version of the kernel of the host
-// from the utsname struct.
-func GetCurrentKernelVersion() (major, minor, patch uint32, err error) {
-	var uname unix.Utsname
-	if err := unix.Uname(&uname); err != nil {
-		return 0, 0, 0, fmt.Errorf("could not get Kernel Version: %v", err)
-	}
-	_, _ = fmt.Fscanf(bytes.NewReader(uname.Release[:]), "%d.%d.%d", &major, &minor, &patch)
-	return major, minor, patch, nil
 }
